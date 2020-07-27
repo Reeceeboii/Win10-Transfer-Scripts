948928f8a8b187c2085f1d6aa3a8b2c5dab9f588
Reece Mercer <reecemercer981@gmail.com>
2020-07-28 00:33:32 +0100

    First version of main and test

---
+++ Main.ps1
@@ -0,0 +1,11 @@
+﻿#Requires -RunAsAdministrator
+
+$script_start = Get-Date
+echo "`nWelcome back Reece! - it's currently $script_start`nStarting...`n"
+
+
+echo "Installing Chocolatey..."
+echo "`tSetting required execution policy..."
+set-ExecutionPolicy AllSigned
+echo "`Downloading and running install.ps1..."
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
---
+++ Test.ps1
@@ -0,0 +1,25 @@
+﻿#Requires -RunAsAdministrator
+
+# Is the Windows sandbox optional feature turned on?
+function sandboxEnabled{
+    [bool](Get-WindowsOptionalFeature -Online | Where-Object FeatureName -eq Containers-DisposableClientVM).State
+}
+
+# Is windows sandbox enabled on this system?
+if(sandboxEnabled) {
+    # Copy the contents of the main script to clipboard
+    Get-Content ./Main.ps1 | clip
+    echo "Main script copied to clipboard, opening VM..."
+    echo "Booting Windows Sandbox..."
+    WindowsSandbox.exe
+} else {
+    # Do I actually want to enable it?
+    $resp = Read-Host -Prompt "Windows Sandbox is not enabled, would you like to enable? [Y/N]: "
+    if($resp.ToLower() -eq "y") {
+        echo "Attempting to enable Windows Sandbox..."
+        # Enable the 'Containers-DisposableClientVM' (sandbox) feature
+        Enable-WindowsOptionalFeature -FeatureName Containers-DisposableClientVM -Online -NoRestart -ErrorAction Stop
+    } else {
+        echo "Exiting..."
+    }
+}
