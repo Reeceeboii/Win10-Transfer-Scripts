2f4555f0054799c87c99f76f83efafa59b26f4c4
Reece Mercer <reecemercer981@gmail.com>
2020-11-09 23:44:23 +0000

    Zipping backup files

--- scripts/Backup.ps1
+++ scripts/Backup.ps1
@@ -12,7 +12,7 @@
     Switch that is enabled if PowerShell aliases are to be backed up
 
     .PARAMETER RainmeterSkins
-    Switch that is enabled if Rainmeter skins are to be backed up
+    Switch that is enabled if Rainmeter skins and their settings are to be backed up
 
     .PARAMETER Clean
     Switch that is enabled if created backups are to be removed from C:\Temp and OneDrive
@@ -40,47 +40,73 @@ param (
 )
 
 $tempDir = "C:\Temp"
-$backupDirName = "WIN10TSBACKUPS"
+$localTemp = Join-Path -Path $PSScriptRoot -ChildPath "backup"
 $consumerOneDrive = Join-Path -Path $env:OneDriveConsumer -ChildPath $backupDirName
+
+if ($Packages -or $Aliases -or $RainmeterSkins) {
+    if (Test-Path -Path $localTemp) {
+        Remove-Item -Force -Recurse $localTemp
+    }
+    New-Item -Path $localTemp -ItemType "directory"
+}
 
 
 # Takes a list of locally installed chocolatey packages and stores them in a log file
 if ($Packages) {
-    if (-not (Test-Path -Path $consumerOneDrive)) {
-        New-Item -Path $env:OneDriveConsumer -Name $backupDirName -ItemType "directory"
-        Write-Host "Created OneDrive backup folder" -ForegroundColor Green
-    }
-    clist -l -idonly -r | Out-File -FilePath ./packages.log
+    clist -l -idonly -r | Out-File -Force -FilePath $localTemp\packages.log
     Write-Host "packages.log file created" -ForegroundColor Green
-    # Upload packages.log to personal OneDrive folder and copy to C:\Temp
-    Copy-Item ./packages.log $consumerOneDrive
-    Copy-Item ./packages.log $tempDir
-    Remove-Item ./packages.log
-    Write-Host "All packages backed up to package.log (C:\Temp & Personal OneDrive backup folder)" -ForegroundColor Green
 }
-
 
 # Takes a list of all PowerShell aliases and stores them in a log file
 if ($Aliases) {
-    Get-Alias | ForEach-Object { "$($_.Name),$($_.Definition)" } | Out-File -FilePath ./aliases.log
+    Get-Alias | ForEach-Object { "$($_.Name),$($_.Definition)" } | Out-File -FilePath $localTemp\aliases.log
     Write-Host "aliases.log file created" -ForegroundColor Green
-    # Upload aliases.log to personal OneDrive folder and copy to C:\Temp
-    Copy-Item ./aliases.log $consumerOneDrive
-    Copy-Item ./aliases.log $tempDir
-    Remove-Item ./aliases.log
-    Write-Host "All aliases backed up to aliases.log (C:\Temp & Personal OneDrive backup folder)" -ForegroundColor Green
 }
 
+# Makes a copy installed Rainmeter skins and settings 
+if ($RainmeterSkins) {
+    $rainmeterSkinsLocal = Join-Path -Path (Resolve-Path -Path "~\Documents") -ChildPath "Rainmeter"
+    if (Test-Path $rainmeterSkinsLocal) {
+        Copy-Item -Force -Recurse -Path $rainmeterSkinsLocal -Destination $localTemp
+    }
+    else {
+        Write-Host "Rainmeter folder doesn't seem to exist?" -ForegroundColor Red
+    }
+}
+
+# if any backups were made
+if ($Packages -or $Aliases -or $RainmeterSkins) {
+    $compress = @{
+        Path             = ".\backup"
+        CompressionLevel = "Fastest"
+        DestinationPath  = "$($localTemp)\backup.zip"
+    }
+    Compress-Archive @compress
+    Write-Host "Backup archive created" -ForegroundColor Green
+
+    Copy-Item $localTemp\packages.log $tempDir -ErrorAction SilentlyContinue
+    Copy-Item $localTemp\aliases.log $tempDir -ErrorAction SilentlyContinue
+    Copy-Item -Force -Recurse -Path $rainmeterSkinsLocal -Destination $tempDir -ErrorAction SilentlyContinue
+    Write-Host "`tLocal ($($tempDir)) copies created" -ForegroundColor Yellow
+    Copy-Item -Force -Path $localTemp\backup.zip -Destination $consumerOneDrive
+    Write-Host "`tOneDrive ($($consumerOneDrive)) copies created" -ForegroundColor Yellow
+
+}
 
 # Delete package and alias logs from local drive and OneDrive
 if ($Clean) {
-    # OneDrive copies
-    if (Test-Path -Path $consumerOneDrive) {
-        Remove-Item $consumerOneDrive\*
-        Write-Host "Uploaded OneDrive files have been cleaned!" -ForegroundColor Green
+    if (Test-Path $localTemp) {
+        Remove-Item -Recurse -Force $localTemp
+        Write-Host "Local backup temp files removed" -ForegroundColor Green
     }
+    # OneDrive files
+    Remove-Item $consumerOneDrive\backup.zip -ErrorAction SilentlyContinue
+    Write-Host "OneDrive zip removed!" -ForegroundColor Green
+
     # Local copies
     Remove-Item $tempDir\packages.log -ErrorAction SilentlyContinue
     Remove-Item $tempDir\aliases.log -ErrorAction SilentlyContinue
     Write-Host "Local .log files have been cleaned!" -ForegroundColor Green
+    Remove-Item -Recurse $tempDir\Rainmeter -ErrorAction SilentlyContinue
+    Write-Host "Local Rainmeter backups have been cleaned!" -ForegroundColor Green
 }
